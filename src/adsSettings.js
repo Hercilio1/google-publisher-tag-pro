8d6178b367da74f6d5e39447d546d831ab35b303
Hercílio M. Ortiz <herciliomartins@gmail.com>
2023-05-02 17:18:14 -0300

    Tmp: Addition of html file to test the lib with bootstrap

--- src/adsSettings.js
+++ src/adsSettings.js
@@ -6,4 +6,5 @@ export const REFRESH_KEY = gptProSettings?.REFRESH_KEY ?? "refresh";
 export const REFRESH_VALUE = gptProSettings?.REFRESH_VALUE ?? "true";
 export const SECONDS_TO_WAIT_AFTER_VIEWABILITY =
   gptProSettings?.SECONDS_TO_WAIT_AFTER_VIEWABILITY ?? 30;
-export const ADS_SELECTOR = gptProSettings?.ADS_SELECTOR ?? ".ad-gpt-pro";
+export const ADS_SELECTOR =
+  gptProSettings?.ADS_SELECTOR ?? ".ad-gpt-pro .ad-gpt-pro-slot";
